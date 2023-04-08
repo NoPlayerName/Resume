546edc5690a882370d4a9f179afda05035cb7ba0
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2023-04-08 13:29:07 +0700

    add funct register and store

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,14 +1,39 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Mail\Message;
 
 class AuthController extends Controller
 {
  
     public function index()
     {
-        return view('auth.index');
+        return view('auth.index', [
+            'title' => 'Login Page'
+        ]);
+    }
+
+    public function register()
+    {
+        return view('auth.register', [
+            'title' => 'Register'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+            $password = md5($request->password);
+            User::create([
+                'username' => $request->username,
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $password,
+            ]);
+
+            return redirect('login');
     }
     
 }
