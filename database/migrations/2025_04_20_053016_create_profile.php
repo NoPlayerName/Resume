60b1624d802e12a6d274347be5c6a64e724084e3
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2025-04-20 15:39:57 +0700

    create migration profile

---
+++ database/migrations/2025_04_20_053016_create_profile.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('role');
+            $table->text('about_me');
+            $table->date('tgl_lahir');
+            $table->string('tmpt_lahir');
+            $table->jsonb('kontak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
